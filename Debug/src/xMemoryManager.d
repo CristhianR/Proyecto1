b4fc6c905c9bb20eb6af11eac6c9c16b264813a9
Antony <antonycr@hotmail.com>
2016-04-13 22:17:36 -0600

    .

---
+++ Debug/src/xMemoryManager.d
@@ -0,0 +1,12 @@
+src/xMemoryManager.o: ../src/xMemoryManager.cpp ../src/xMemoryManager.h \
+ ../src/Pelota.h ../src/xObject.h ../src/Animal.h ../src/RC.h
+
+../src/xMemoryManager.h:
+
+../src/Pelota.h:
+
+../src/xObject.h:
+
+../src/Animal.h:
+
+../src/RC.h:
